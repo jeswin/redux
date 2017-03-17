48d195f33f652f29a43dfc62ed01dcf167da6ef9
Jeswin Kumar <jeswinpk@agilehead.com>
2017-03-17 20:54:28 +0530

    Update index.js
    Remove constants.

--- examples/redux-lite/src/actions/index.js
+++ examples/redux-lite/src/actions/index.js
@@ -1,7 +1,3 @@
-export const REQUEST_POSTS = "REQUEST_POSTS";
-export const RECEIVE_POSTS = "RECEIVE_POSTS";
-export const SELECT_REDDIT = "SELECT_REDDIT";
-export const INVALIDATE_REDDIT = "INVALIDATE_REDDIT";
 import { getState, updateState } from "../store";
 
 export function selectReddit(reddit) {
