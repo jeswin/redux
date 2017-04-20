7652d27e5d0794bb633fd19d001810fb397a66bc
Jeswin Kumar <jeswinpk@agilehead.com>
2017-04-20 18:24:30 +0530

    Allow action type names

--- examples/redux-lite/src/store.js
+++ examples/redux-lite/src/store.js
@@ -3,26 +3,39 @@ import { createStore as reduxCreateStore } from "redux";
 const initialState = {
   selectedReddit: "reactjs",
   postsByReddit: {}
-}
+};
 
 let store;
 
+const ACTION_PREFIX = "$$RX";
+
 export function createStore() {
   const args = [].slice.call(arguments);
-  store = reduxCreateStore.apply(undefined, [reducer, initialState].concat(args));
+  store = reduxCreateStore.apply(
+    undefined,
+    [
+      reducer,
+      initialState,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    ].concat(args)
+  );
   return store;
 }
 
 function reducer(state, action) {
-  return action && action.type === "REPLACE_STATE" ? action.state : state;
+  return action && action.type.startsWith(ACTION_PREFIX) ? action.state : state;
 }
 
 export function getState(selector) {
   return selector ? selector(store.getState()) : store.getState();
 }
 
-export function updateState(property, update) {
+export function updateState(property, update, actionType) {
   const state = store.getState();
   const newState = { ...state, [property]: update(state[property]) };
-  store.dispatch({ type: "REPLACE_STATE", state: newState });
+  store.dispatch({
+    type: actionType ? `${ACTION_PREFIX}.${actionType}` : ACTION_PREFIX,
+    state: newState
+  });
 }
